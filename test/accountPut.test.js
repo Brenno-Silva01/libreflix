da9c2fd55a40548aae8af6ecd9ed4beb4e613b1c
Brenno-Silva01 <brenno877@gmail.com>
2025-01-14 20:18:21 -0300

    Adição de testes para o método accountPut

---
+++ test/accountPut.test.js
@@ -0,0 +1,132 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const app = require('../app'); 
+const User = require('../models/User'); 
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Teste do método accountPut', () => {
+  let userStub;
+
+  beforeEach(() => {
+    // Cria um stub para simular o método findById
+    userStub = sinon.stub(User, 'findById');
+  });
+
+  afterEach(() => {
+    // Restaura os métodos originais
+    sinon.restore();
+  });
+
+  it('CT1: password presente, válido e confirmado (VVV)', (done) => {
+    const mockUser = { save: sinon.stub().yields(null) };
+    userStub.yields(null, mockUser);
+
+    chai
+      .request(app)
+      .put('/account')
+      .send({ password: '1234', confirm: '1234' })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(mockUser.save.calledOnce).to.be.true;
+        done();
+      });
+  });
+
+  it('CT2: password ausente (FVV)', (done) => {
+    chai
+      .request(app)
+      .put('/account')
+      .send({ email: 'user@example.com' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.text).to.include('Este e-mail não é válido.');
+        done();
+      });
+  });
+
+  it('CT3: password presente, mas muito curta (VFV)', (done) => {
+    chai
+      .request(app)
+      .put('/account')
+      .send({ password: '123', confirm: '123' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.text).to.include('A senha precisa ter pelo menos 4 caracteres.');
+        done();
+      });
+  });
+
+  it('CT4: password presente, mas confirmação falha (VVF)', (done) => {
+    chai
+      .request(app)
+      .put('/account')
+      .send({ password: '1234', confirm: '12345' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.text).to.include('As senhas inseridas não conferem.');
+        done();
+      });
+  });
+
+  it('CT5: Erro no e-mail (FFV)', (done) => {
+    chai
+      .request(app)
+      .put('/account')
+      .send({ email: 'email_invalido' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.text).to.include('Este e-mail não é válido.');
+        done();
+      });
+  });
+
+  it('CT6: E-mail vazio (FVF)', (done) => {
+    chai
+      .request(app)
+      .put('/account')
+      .send({ email: '' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.text).to.include('O campo "Email" precisa ser preenchido.');
+        done();
+      });
+  });
+
+  it('CT7: Erro de duplicidade (VV)', (done) => {
+    const mockUser = { save: sinon.stub().yields({ code: 11000 }) };
+    userStub.yields(null, mockUser);
+
+    chai
+      .request(app)
+      .put('/account')
+      .send({ password: '1234', confirm: '1234' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.text).to.include('E-mail ou username inseridos já estão associados com outra conta.');
+        done();
+      });
+  });
+
+  it('CT8: Atualização de perfil com sucesso (FVFF)', (done) => {
+    const mockUser = { save: sinon.stub().yields(null) };
+    userStub.yields(null, mockUser);
+
+    chai
+      .request(app)
+      .put('/account')
+      .send({
+        email: 'user@example.com',
+        username: 'user',
+        name: 'User Name',
+        location: 'User Location',
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(mockUser.save.calledOnce).to.be.true;
+        done();
+      });
+  });
+});
